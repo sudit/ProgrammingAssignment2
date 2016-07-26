b0326b3022152f32bbb82186a5465f78d439a984
sudit <sudit@buffalo.edu>
2016-07-26 08:51:38 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function creates a "special" matrix that will be used in cacheSolve
+## to determine if an invers already exists or it needs to be computed
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <-function (z= matrix()) {
+    invmat<- NULL
+    inv <-function (w) {
+        z <<- w
+        invmat <<- NULL
+    }
+    getmat <- function () z
+    setinv <- function (solve) invmat <<- solve
+    getinv <- function () invmat
+    list (inv=inv, getmat=getmat, setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Computes Matrix Inverse if needed or recalls one if it already exists
+cacheSolve <- function (z,...) {
+    invmat<-z$getinv ()
+    if(!is.null(invmat)) {
+        message ("getting cached inverse matrix")
+        return (invmat)
+    }
+    datainv <- z$getmat()
+    invmat <- solve(datainv,...)
+    z$setinv(invmat)
+    invmat
 }
